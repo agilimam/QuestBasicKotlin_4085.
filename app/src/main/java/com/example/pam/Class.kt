82f5d6bbbfa03080320d71107e60ce6f19ae905c
agilimam <agilimamalhafizi04@gmail.com>
2024-10-13 12:54:50 +0700

    memanggik fungsi contact val id dan var email

--- app/src/main/java/com/example/pam/Class.kt
+++ app/src/main/java/com/example/pam/Class.kt
@@ -3,3 +3,9 @@ package com.example.pam
 class motor()
 
 class Contact(val id: Int, var email: String)
+
+fun main(){
+    val contact = Contact(1,"mary@gmail.com")
+    //print the value of the property: email
+    println(contact.email)
+}
