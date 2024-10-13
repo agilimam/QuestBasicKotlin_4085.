f68662a943215075faed03d01c2f976d2ee580d3
agilimam <agilimamalhafizi04@gmail.com>
2024-10-13 15:50:31 +0700

    Menerapkan fungsi tanpa parameter, fungsi dengan parameter, dan demonstrasi penggunaan named arguments

---
+++ app/src/main/java/com/example/pam/Functions.kt
@@ -0,0 +1,22 @@
+package com.example.pam
+
+fun withoutParameter() {
+    println("== withoutParameter ==")
+    println("Hello, World!")
+}
+
+fun withParameter(name: String) {
+    println()
+    println("== withParameter ==")
+    println("Hello, $name!")
+}
+
+//named argument
+//named argument merupakan cara untuk memanggil fungsi dengan menyebutkan nama parameter
+fun withNamedArgument(name: String, age: Int) {
+    println()
+    println("== withNamedArgument ==")
+    println("Hello, $name! You are $age years old.")
+}
+
+
