a707c92911925f91eac567799f872617e6cc343c
agilimam <agilimamalhafizi04@gmail.com>
2024-10-13 13:22:30 +0700

    Codingan di kelas

---
+++ app/src/main/java/com/example/pam/Typedata.kt
@@ -0,0 +1,44 @@
+package com.example.pam
+
+
+fun main() {
+
+    //byte
+    val byteValue: Byte = 10
+    println("Byte: $byteValue")
+
+    //short
+    val shortValue: Short = 20
+    println("Short: $shortValue")
+
+    //int
+    val intValue: Int = 30
+    println("int: $intValue")
+
+    //Long
+    val longValue: Long = 40
+    println("Long: $longValue")
+
+    // Float
+    val floatValue: Float = 50.6F
+    println("Float: $floatValue")
+
+    //double
+    val doubleValue: Double = 60.6
+    println("Double: $doubleValue")
+
+    //char
+    val charValue: Char = 'A'
+    println("Char: $charValue")
+
+    //Boolean
+    val booleanValue: Boolean  = true
+    println("Boolean: $booleanValue")
+
+    // String
+    var stringValue: String = "Hello, World!"
+    println("String: $stringValue")
+
+    stringValue = "Hello, Kotlin"
+    println("String: $stringValue")
+}
